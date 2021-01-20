7d819cdf97b48e7d95915d783f869dfbdd40f4c3
oyasumi <yadensann@gmail.com>
2021-01-20 11:57:47 -0700

    adding markers to map

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoieWFkZW5zYW5uIiwiYSI6ImNranB5a3ByczBsbWQycW15MWxqbHgxNTQifQ.Z_r4rsFDVyWs7Fe_pzDcfA";
---
+++ static/js/logic.js
@@ -0,0 +1,109 @@
+// Visualizing Data with Leaflet
+
+// Significant Earthquake GeoJSON URL variables
+var earthquake_url=  'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson#'
+
+// Create LayerGroup for earthquakes
+var earthquake_layer= new L.LayerGroup();
+
+// Variables for tile layers
+var satellite_map = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+});
+
+var light_mode= L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+});
+
+// Define baseMaps Object to Hold Base Layers
+var baseMaps = {
+    "Satellite": satellite_map,
+    "Light": light_mode,
+};
+
+// Create Overlay Object to Hold Overlay Layers
+var overlayMaps = {
+    "Earthquakes": earthquake_layer
+};
+
+// Create Map, Passing In satelliteMap & earthquakes as Default Layers to Display on Load
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 2,
+    layers: [satellite_map, earthquake_layer]
+});
+
+// Adding layer control to map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+// Retrieve earthquake data with D3
+d3.json(earthquake_url, function(earthquake_data) {
+    // Function for marker size based on magnitude of earthquake
+    function marker_size(magnitude) {
+        if(magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 3;
+    }
+    function marker_style(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: choose_color(feature.properties.mag),
+            color: "#000000",
+            radius: marker_size(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+          };
+        }
+    // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+    function choose_color(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+            return "#581845";
+        case magnitude > 4:
+            return "#900C3F";
+        case magnitude > 3:
+            return "#C70039";
+        case magnitude > 2:
+            return "#FF5733";
+        case magnitude > 1:
+            return "#FFC300";
+        default:
+            return "#DAF7A6";
+        }
+    }
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+        // Run the onEachFeature function once for each piece of data in the array
+    L.geoJSON(earthquake_data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: marker_style,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(("<h4>Location: " + feature.properties.place + 
+            "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+            "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>")
+        )}
+}).addTo(earthquake_layer)
+earthquake_layer.addTo(myMap);
+})
+    //         radius: radius_size(earthquake_data.properties.mag),
+    //         color: circle_color(earthquake_data.properties.mag),
+    //         fillOpacity: 1
+    //     });
+    //     },
+    //     onEachFeature: 
+    // });
+
+    // // Sending our earthquakes layer to the createMap function
+    // createMap(earthquakes);
+    // });
+
